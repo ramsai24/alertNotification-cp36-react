2401056370e80a65a406762937942b1b5e0d1d5a
jnrsai24 <jnrsai24@gmail.com>
2023-07-14 03:41:42 +0000

    6 test casses pending out of 15

--- src/components/AlertNotifications/index.js
+++ src/components/AlertNotifications/index.js
@@ -1,1 +1,36 @@
 // Write your code here
+import Notification from '../Notification'
+import './index.css'
+
+const notificationsList = [
+  {
+    heading: 'Success',
+    description: 'You can access all the files in the folder',
+  },
+  {
+    heading: 'Error',
+    description:
+      'Sorry, you are not authorized to have access to delete the file',
+  },
+  {
+    heading: 'Warning',
+    description: 'Viewers of the file can see comments and suggestions',
+  },
+  {
+    heading: 'Info',
+    description: 'Anyone on the internet can view these files',
+  },
+]
+
+const AlertNotifications = () => (
+  <div className="app-container">
+    <h1>Alert Notifications</h1>
+    <ul>
+      {notificationsList.map(each => (
+        <Notification key={each.heading}>{each}</Notification>
+      ))}
+    </ul>
+  </div>
+)
+
+export default AlertNotifications
--- src/components/Notification/index.js
+++ src/components/Notification/index.js
@@ -1,1 +1,64 @@
 // Write your code here
+import {AiFillCheckCircle} from 'react-icons/ai'
+import {RiErrorWarningFill} from 'react-icons/ri'
+import {MdWarning, MdInfo} from 'react-icons/md'
+import {GrFormClose} from 'react-icons/gr'
+
+import './index.css'
+
+const Notification = props => {
+  const {children} = props
+  console.log(children)
+  const {heading, description} = children
+
+  switch (heading) {
+    case 'Success':
+      return (
+        <div className="notification-item">
+          <AiFillCheckCircle />
+          <div className="heading-description-container">
+            <h1 className="heading">{heading}</h1>
+            <p className="description">{description}</p>
+          </div>
+          <GrFormClose className="close-icon" />
+        </div>
+      )
+    case 'Error':
+      return (
+        <div className="notification-item">
+          <RiErrorWarningFill />
+          <div className="heading-description-container">
+            <h1 className="heading">{heading}</h1>
+            <p className="description">{description}</p>
+          </div>
+          <GrFormClose className="close-icon" />
+        </div>
+      )
+    case 'Warning':
+      return (
+        <div className="notification-item">
+          <MdWarning />
+          <div className="heading-description-container">
+            <h1 className="heading">{heading}</h1>
+            <p className="description">{description}</p>
+          </div>
+          <GrFormClose className="close-icon" />
+        </div>
+      )
+    case 'Info':
+      return (
+        <div className="notification-item">
+          <MdInfo />
+          <div className="heading-description-container">
+            <h1 className="heading">{heading}</h1>
+            <p className="description">{description}</p>
+          </div>
+          <GrFormClose className="close-icon" />
+        </div>
+      )
+    default:
+      return null
+  }
+}
+
+export default Notification
